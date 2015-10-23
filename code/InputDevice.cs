3cb4a083c3b5239d099289582fed7910fdf61bdb
Yvan J.W. HINIGER <YHiniger@users.noreply.github.com>
2015-10-23 03:12:20 +0200

    Added raw mouse support.

---
+++ code/InputDevice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace ManagedX.Input
+{
+
+	// Ideally, an input device should be identifiable by its device type and index.
+	
+
+	/// <summary>Base class for all managed input devices.</summary>
+	public abstract class InputDevice : Design.IInputDevice
+	{
+
+
+		/// <summary></summary>
+		/// <returns></returns>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate" )]
+		protected static Exception GetLastWin32Exception()
+		{
+			var errorCode = Marshal.GetLastWin32Error();
+			var ex = Marshal.GetExceptionForHR( errorCode );
+			if( ex == null )
+				ex = new System.ComponentModel.Win32Exception( errorCode );
+			return ex;
+		}
+
+
+		private GameControllerIndex index;
+
+
+
+		/// <summary>Constructor.</summary>
+		/// <param name="controllerIndex">The index of the device; must be unique per device type (<see cref="InputDeviceType"/>).</param>
+		internal InputDevice( GameControllerIndex controllerIndex )
+		{
+			index = controllerIndex;
+		}
+
+
+
+		/// <summary>Gets the index of this input device.</summary>
+		public GameControllerIndex Index { get { return index; } }
+
+
+		/// <summary>When overridden, gets a value indicating the type of the input device.</summary>
+		public abstract InputDeviceType DeviceType { get; }
+
+
+		/// <summary>When overridden, gets a value indicating whether the input device is disconnected.</summary>
+		public abstract bool Disconnected { get; }
+
+
+		/// <summary>When overridden, updates the state of the input device.</summary>
+		/// <param name="time">The current time.</param>
+		public abstract void Update( TimeSpan time );
+
+	}
+
+}
