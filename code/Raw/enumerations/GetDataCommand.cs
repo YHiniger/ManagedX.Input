43e219078921a11c5db74c096d17b025ec3e5e78
Yvan J.W. HINIGER <YHiniger@users.noreply.github.com>
2015-10-23 03:10:40 +0200

    Added raw mouse support.

---
+++ code/Raw/enumerations/GetDataCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ManagedX.Input.Raw
+{
+	
+	/// <summary>Enumerates commands used by the GetRawInputData function.</summary>
+	[System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Design", "CA1008:EnumsShouldHaveZeroValue" )]
+	internal enum GetDataCommand : int
+	{
+
+		/// <summary>Get the raw data from the <see cref="RawInput"/> structure.</summary>
+		Input = 0x10000003,
+
+		/// <summary>Get the header information from the <see cref="RawInput"/> structure.</summary>
+		Header = 0x10000005
+
+	}
+
+}
