188b3efddda1132b408e76276576e6fbab766d4e
Yvan J.W. HINIGER <YHiniger@users.noreply.github.com>
2016-09-08 02:13:11 +0200

    Added extension methods to read/write a Vibration structure from/to a stream.

---
+++ BinaryReaderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+
+namespace ManagedX.Input.XInput
+{
+
+	/// <summary>Provides an extension method to read a <see cref="Vibration"/> structure from a stream using a <see cref="BinaryReader"/>.</summary>
+	public static class BinaryReaderExtensions
+	{
+
+		/// <summary>Reads a <see cref="Vibration"/> structure from a stream and returns it.</summary>
+		/// <param name="reader">A reader; must not be null.</param>
+		/// <returns>Returns the read <see cref="Vibration"/> structure.</returns>
+		public static Vibration ReadVibration( this BinaryReader reader )
+		{
+			if( reader == null )
+				throw new ArgumentNullException( "reader" );
+
+			Vibration vibration;
+			vibration.leftMotorSpeed = reader.ReadUInt16();
+			vibration.rightMotorSpeed = reader.ReadUInt16();
+			return vibration;
+		}
+
+	}
+
+}
---
+++ BinaryWriterExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+
+namespace ManagedX.Input.XInput
+{
+
+	/// <summary>Provides an extension method to write a <see cref="Vibration"/> structure to a stream using a <see cref="BinaryWriter"/>.</summary>
+	public static class BinaryWriterExtensions
+	{
+
+		/// <summary>Writes a <see cref="Vibration"/> structure to a stream.</summary>
+		/// <param name="writer">The writer; must not be null.</param>
+		/// <param name="vibration">A <see cref="Vibration"/> structure.</param>
+		public static void Write( this BinaryWriter writer, Vibration vibration )
+		{
+			if( writer == null )
+				throw new ArgumentNullException( "writer" );
+			writer.Write( vibration.leftMotorSpeed );
+			writer.Write( vibration.rightMotorSpeed );
+		}
+
+	}
+
+}
